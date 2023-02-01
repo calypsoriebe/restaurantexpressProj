c9eb896ca8f0f9c41bc3dfd39c4ec5644ad5b166
Calypso Riebe <100364919+calypsoriebe@users.noreply.github.com>
2023-02-01 13:04:33 -0500

    made 404 route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
+//homepage route
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
-
+//404 route BELOW homepage so doesnt overwrite it
+app.get("*", (req, res) => {
+  //the .status(404) makes it legit return an error code
+  res.status(404).send(<h1>ERORR 404: PAGE NOT FOUND</h1>);
+});
 app.listen(process.env.PORT);
