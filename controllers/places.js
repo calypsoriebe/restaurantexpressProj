f42015069f25cffac8ed1b45f819b7cb15978344
Calypso Riebe <100364919+calypsoriebe@users.noreply.github.com>
2023-02-01 13:16:58 -0500

    added places controller

---
+++ controllers/places.js
@@ -0,0 +1,9 @@
+//creating a router
+const router = require("express").Router();
+
+//making an index page
+router.get("/", (req, res) => {
+  res.send("GET /places");
+});
+//will be exported
+module.exports = router;
--- index.js
+++ index.js
@@ -2,13 +2,21 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+
+//this imports the router
+// /places after app.use sets all routes in controller relative to /places so /places will be in paths within controller
+app.use("/places", require("./controllers/places"));
+
 //homepage route
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
+
 //404 route BELOW homepage so doesnt overwrite it
 app.get("*", (req, res) => {
   //the .status(404) makes it legit return an error code
   res.status(404).send(<h1>ERORR 404: PAGE NOT FOUND</h1>);
 });
+
+//tells app what to listen to
 app.listen(process.env.PORT);
