82fcf87644c2a6148354e9295c4e6ddc3386758e
Calypso Riebe <100364919+calypsoriebe@users.noreply.github.com>
2023-02-01 13:49:04 -0500

    added a default view

---
+++ views/default.jsx
@@ -0,0 +1,16 @@
+//default views rake repetitive html that every page on your app will have so then you can write content of page without creating the html skeleton evertime
+//importing react
+const React = require('react')
+
+function Def (html) {
+    return <html>
+        <head>
+            <title>Title</title>
+        </head>
+        <body>
+            {html.children}
+        </body>
+    </html>
+}
+
+module.exports = Def
